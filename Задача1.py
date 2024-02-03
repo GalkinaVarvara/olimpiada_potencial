4dd6ec48164202c1727e1938e274bf75d65d10d8
Varvara Galkina <galkinavi2006@gmail.com>
2024-02-03 15:59:22 +0300

    add file

---
+++ Задача1.py
@@ -0,0 +1,12 @@
+import csv
+with open('game.csv',encoding='utf-8') as f:
+    reader = csv.reader(f,delimiter='$')
+    ans = list(reader)
+    for i in range(len(ans)):
+        if '55'in ans[i][-2]:
+           print(f'У персонажа {ans[i][1]} в игре {ans[i][0]} нашлась ошибка с кодом: {ans[i][2]}. Дата фиксации: {ans[i][3]}')
+           ans[i][-2] = 'Done'
+           ans[i][-1] = '0000-00-00'
+with open('game_new.csv', 'w', encoding='utf8', newline='') as f:
+    w = csv.writer(f)
+    w.writerows(ans)
