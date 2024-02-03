b5e06554dec7666f47c756694d7c9ea9fa4ee8af
Varvara Galkina <galkinavi2006@gmail.com>
2024-02-03 16:49:59 +0300

    add file

--- Задача1.py
+++ Задача1.py
@@ -8,5 +8,5 @@ with open('game.csv',encoding='utf-8') as f:
            ans[i][-2] = 'Done'
            ans[i][-1] = '0000-00-00'
 with open('game_new.csv', 'w', encoding='utf8', newline='') as f:
-    w = csv.writer(f)
+    w = csv.writer(f,delimiter='$')
     w.writerows(ans)
